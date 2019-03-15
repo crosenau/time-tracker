16b10527a94e2f57e22e85e17907c89720474482
crosenau <rosenauc74@hotmail.com>
2019-03-15 15:49:40 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const api = require('./routes/api/users');
+const initDb = require('./db').initDb;
+const keys = require('./config/keys');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+initDb()
+  .then(db => {
+    console.log('Successfully connected to database');
+    
+    //app.use(passport.initialize());
+    //require('./config/passport')(passport);
+
+    //app.use('/api', api);
+
+    const port = keys.port || 5000;
+
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  })
+  .catch(err => console.log(err));
