9c15ea30e458ecb9a0b28958e845ce5c898e6250
crosenau <rosenauc74@hotmail.com>
2019-03-16 17:36:13 -0700

    added login and registration input validation

---
+++ validation/login.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateRegisterInput(data) {
+  const errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+  if (isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  } else if (!validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+};
