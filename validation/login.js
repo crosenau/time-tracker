357fbfa0c70fc8f521d42992f724b84816f3c5b6
crosenau <rosenauc74@hotmail.com>
2019-04-05 15:43:53 -0700

    changed error message wording

--- validation/login.js
+++ validation/login.js
@@ -10,13 +10,13 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : '';
 
   if (isEmpty(data.email)) {
-    errors.email = 'Email field is required';
+    errors.email = 'Email is required';
   } else if (!validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
 
   if (isEmpty(data.password)) {
-    errors.password = 'Password field is required';
+    errors.password = 'Password is required';
   }
 
   return {
