6627211472a3ed2472744b6e817178bf84d25687
crosenau <rosenauc74@hotmail.com>
2019-04-04 20:34:00 -0700

    password and password2 validation now distinguish missing or invalid inputs

--- validation/register.js
+++ validation/register.js
@@ -12,28 +12,24 @@ module.exports = function validateRegisterInput(data) {
   data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   if (validator.isEmpty(data.name)) {
-    errors.name = 'Name field is required';
+    errors.name = 'Name is required';
   }
 
   if (validator.isEmpty(data.email)) {
-    errors.email = 'Email field is required';
+    errors.email = 'Email is required';
   } else if (!validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
 
   if (validator.isEmpty(data.password)) {
     errors.password = 'Password is required';
+  } else if (!validator.isLength(data.password, { min:6, max: 30 })) {
+    errors.password = 'Password must be between 6 and 30 characters long';
   }
   
   if (validator.isEmpty(data.password2)) {
     errors.password2 = 'Confirm Password is required';
-  }
-
-  if (!validator.isLength(data.password, { min:6, max: 30 })) {
-    errors.password = 'Password must be at least 6 characters long';
-  }
-
-  if (!validator.equals(data.password, data.password2)) {
+  } else if (!validator.equals(data.password, data.password2)) {
     errors.password2 = 'Passwords must match'
   }
 
