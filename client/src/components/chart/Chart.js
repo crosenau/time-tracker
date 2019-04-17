dc6ac26d732becfb92bae7ad6663ef1e2fa01f9b
crosenau <rosenauc74@hotmail.com>
2019-04-16 20:59:24 -0700

    started implementing svg chart

--- client/src/components/chart/Chart.js
+++ client/src/components/chart/Chart.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
 import {
   getTasks,
   updateChartSettings,
   toggleChartSettings
 } from '../../actions/chartActions';
+
+import * as d3 from 'd3';
 
 import ChartSettings from '../settings/ChartSettings';
 
@@ -14,6 +15,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Chart.module.css';
 
 class Chart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.node = React.createRef();
+    this.createChart = this.createChart.bind(this);
+  }
+
   componentDidMount() {
     const { startDate, endDate } = this.props.chart;
 
@@ -21,6 +29,55 @@ class Chart extends Component {
       startDate,
       endDate
     });
+  }
+  
+  componentDidUpdate() {
+    if (!this.props.chart.displaySettings) {
+      this.createChart();
+      console.log(this.props.chart.tasks);
+    }
+  }
+
+  createChart() {
+    const node = this.node.current;
+    const width = Number(getComputedStyle(node).width.replace('px', ''));
+    const height = Number(getComputedStyle(node).height.replace('px', ''));
+
+    const dataset = this.props.chart.tasks;
+
+    const xScale = d3.scaleLinear()
+      .domain([0, dataset.length])
+      .range([0, width]);
+    
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset, d => d.taskLength)])
+      .range([height, 0]);
+
+    // Delete any existing svgs
+    d3.select(node).selectAll('svg').remove();
+
+    const chart = d3.select(node)
+      .append('svg')
+      .attr('id', 'chartSvg')
+      .attr('width', width)
+      .attr('height', height);
+    
+    //bars
+    const barWidth = width / dataset.length;
+
+    chart.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('dataTaskName', d => d.taskName)
+      .attr('dataTaskLength', d => d.taskLength)
+      .attr('dataCompletedAt', d => d.completedAt)
+      .attr('x', (d, i) => xScale(i))
+      .attr('y', d => yScale(d.taskLength))
+      .attr('width', barWidth)
+      .attr('height', d => height - yScale(d.taskLength))
+      .attr('fill', '#000000');
   }
 
   render() {
@@ -35,6 +92,7 @@ class Chart extends Component {
         </div>
 
         <h4>{this.props.chart.startDate.toLocaleDateString()} - {this.props.chart.endDate.toLocaleDateString()}</h4>
+        <div id={styles.svgContainer} ref={this.node} />
       </div>
     );
   }
