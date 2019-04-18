7b603cbf2d7a7a39e95d61ba4627349e1c86f33b
crosenau <rosenauc74@hotmail.com>
2019-04-17 21:46:23 -0700

    tasks now requested at proper times. SVG container div no longer expands on render

--- client/src/components/chart/Chart.js
+++ client/src/components/chart/Chart.js
@@ -31,10 +31,24 @@ class Chart extends Component {
     });
   }
   
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
+    const { chart } = this.props;
+    const prevChart = prevProps.chart;
+
+    if (
+      prevChart.startDate !== chart.startDate
+      || prevChart.endDate !== chart.endDate
+    ) {
+      const { startDate, endDate } = chart;
+
+      this.props.getTasks({
+        startDate,
+        endDate
+      });
+    }
+
     if (!this.props.chart.displaySettings) {
-      this.createChart();
-      console.log(this.props.chart.tasks);
+      //this.createChart();
     }
   }
 
@@ -42,28 +56,69 @@ class Chart extends Component {
     const node = this.node.current;
     const width = Number(getComputedStyle(node).width.replace('px', ''));
     const height = Number(getComputedStyle(node).height.replace('px', ''));
+    const padding = 4;
 
-    const dataset = this.props.chart.tasks;
+    // consolidate tasks into individual days with total times for each task performed that day
+    const dataset = this.props.chart.tasks
+      .filter(task => task.taskName !== this.props.chart.filter)
+      .map(task => {
+        const msInDay = 1000 * 60 * 60 * 24;
+
+        return {
+          taskName: task.taskName,
+          date: task.completedAt.getTime() / msInDay,
+        };
+      })
+
+    /*
+    dataset = 
+      [
+        {
+          date,
+          TaskName: totalTime
+          TaskName2: totalTime
+          ...
+        }
+      ]
+     */
+
+    const uniqueTasks = Array.from(
+      new Set(
+        dataset
+          .map(task => task.taskName)
+      )
+    );
+
+    const colorScale = d3.scaleLinear()
+      .domain([0, (uniqueTasks.length - 1) / 2, uniqueTasks.length - 1])
+      .range(['#422c42', '#456accfe', '#25dbccff']);
 
     const xScale = d3.scaleLinear()
-      .domain([0, dataset.length])
-      .range([0, width]);
+      .domain([
+        d3.min(dataset, d => d.completedAt.getTime()),
+        d3.max(dataset, d => d.completedAt.getTime())
+      ])
+      .range([padding, width - padding]);
     
     const yScale = d3.scaleLinear()
       .domain([0, d3.max(dataset, d => d.taskLength)])
-      .range([height, 0]);
+      .range([height - padding, padding]);
 
     // Delete any existing svgs
     d3.select(node).selectAll('svg').remove();
 
+    const tooltip = d3.select(node)
+      .append('div')
+      .attr('id', 'tooltip');
+
     const chart = d3.select(node)
       .append('svg')
       .attr('id', 'chartSvg')
-      .attr('width', width)
-      .attr('height', height);
+      .attr('width', width - padding)
+      .attr('height', height - padding);
     
     //bars
-    const barWidth = width / dataset.length;
+    const barWidth = width / dataset.length - 2;
 
     chart.selectAll('rect')
       .data(dataset)
@@ -73,11 +128,21 @@ class Chart extends Component {
       .attr('dataTaskName', d => d.taskName)
       .attr('dataTaskLength', d => d.taskLength)
       .attr('dataCompletedAt', d => d.completedAt)
-      .attr('x', (d, i) => xScale(i))
+      .attr('x', (d, i) => xScale(d.completedAt.getTime()))
       .attr('y', d => yScale(d.taskLength))
       .attr('width', barWidth)
       .attr('height', d => height - yScale(d.taskLength))
-      .attr('fill', '#000000');
+      .attr('fill', d => colorScale(uniqueTasks.indexOf(d.taskName)))
+      /*
+      .on('mousemove', d => {
+        const x = `${d3.event.clientX + 16}px`;
+        const y = `${d3.event.clientY + 8}px`;
+        const data = [
+          d.completedAt.toDateString(),
+          d.completedTasks
+        ];
+      })
+      */
   }
 
   render() {
@@ -91,8 +156,11 @@ class Chart extends Component {
           </button>
         </div>
 
-        <h4>{this.props.chart.startDate.toLocaleDateString()} - {this.props.chart.endDate.toLocaleDateString()}</h4>
         <div id={styles.svgContainer} ref={this.node} />
+        <div id={styles.dateRange}>
+          <span>{this.props.chart.startDate.toDateString()}</span>
+          <span>{this.props.chart.endDate.toDateString()}</span>
+        </div>
       </div>
     );
   }
